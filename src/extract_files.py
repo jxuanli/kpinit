bda40fa1fef36353f3fb581ea695b092a42f4de3
jxuanli <junxua01@student.ubc.ca>
2025-02-15 20:13:35 -0800

    stuff

--- src/extract_files.py
+++ src/extract_files.py
@@ -37,27 +37,29 @@ def extract_vmlinux():
 generate settings.json file if does not exist, otherwise use the existing settings
 """
 def extract_chall_settings(wp_path):
+    settings_fpath = os.path.join(wp_path, CHALL_SETTING)
     if not os.path.exists(settings_fpath):
         settings = {
             BZIMAGE: BZIMAGE,
             RAMFS: RAMFS,
             RUN_SH: RUN_SH,
             VMLINUX: None,
-            VULN_KO: None
+            VULN_KO: None,
             LIBSLUB: None, 
             MODULE_NAME: None,
         }
         name = "vmlinux"
         if is_in_cwd(name):
             settings[VMLINUX] = name 
-        path = "~/Tools/libslub/libslub.py" # default
+        path = os.path.expanduser("~/Tools/libslub/libslub.py") # default
         if os.path.exists(path):
-            names[LIBSLUB] = path
+            settings[LIBSLUB] = path
         for fname in os.listdir(os.getcwd()):
             if fname.endswith(".ko"):
                 settings[VULN_KO] = fname
                 info(f"found ../{fname}")
                 break
-        f = open(os.path.join(wp_path, CHALL_SETTING), "w")
-        json.dump(names, f, indent=4)
-    check_settings(settings_fpath)
+        f = open(settings_fpath, "w")
+        json.dump(settings, f, indent=4)
+        f.flush()
+    check_settings()
